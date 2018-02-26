d67c2a6da7214131b6bed67c0aef7dccb8a91a70
Comput3rZ <32485726+Comput3rZ@users.noreply.github.com>
2018-02-26 12:57:52 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const auth = firebase.auth();
+function supin() {
+  const email = document.getElementById('email').value;
+const password = document.getElementById('password').value;
+const txtemail = email
+const txtpass = password
+const promise = auth.signInWithEmailAndPassword(email, txtpass);
+promise.catch(e => console.log(e.message));
+  console.log("email is:");
+  console.log(email, txtemail);
+    console.log("password is:");
+  console.log(password, txtpass);
+}
+function sup() {
+  const email = document.getElementById('email').value;
+const password = document.getElementById('password').value;
+const txtemail = email
+const txtpass = password
+const promise = auth.createUserWithEmailAndPassword(email, txtpass);
+promise.catch(e => console.log(e.message));
+  console.log("email is:");
+  console.log(email, txtemail);
+    console.log("password is:");
+  console.log(password, txtpass);
+}
